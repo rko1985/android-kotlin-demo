6192f79304b161fa0853643d6dbe74e177b51bb4
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-03-09 11:06:59 -1000

    creating a basic counter app

--- app/src/main/java/com/myappcompany/russell/kotlinfun/MainActivity.kt
+++ app/src/main/java/com/myappcompany/russell/kotlinfun/MainActivity.kt
@@ -2,11 +2,33 @@ package com.myappcompany.russell.kotlinfun
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var count = 0
+
+    fun add(view: View){
+        count++
+        countTextView.text = count.toString()
+
+    }
+
+    fun reset(view: View){
+        count = 0
+        countTextView.text = count.toString()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var countTextView = findViewById<TextView>(R.id.countTextView)
+        countTextView.text = "0"
+
+
     }
 }
